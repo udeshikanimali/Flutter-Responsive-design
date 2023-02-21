9ce64f7be7fc04572e83867cba461a467f223213
Udeshika <udeshikanimali644@gmail.com>
2023-02-21 17:31:27 +0530

    web and mobile layouts

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:test_app/screens/mobile_screen_layout.dart';
+import 'package:test_app/screens/responsive.dart';
+import 'package:test_app/screens/web_screen_layout.dart';
+
+void main() {
+  //runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      builder: (context) => MyApp(), // Wrap your app
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Responsive Design',
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      debugShowCheckedModeBanner: false,
+      
+      home: const ResponsiveLayout(mobileScreenLayout: MobileScreenLayout(), webScreenLayout: WebScreenLayout()),
+    );
+  }
+}
+
+
